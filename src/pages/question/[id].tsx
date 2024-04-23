99990c6568267a764ae5effcf97791b2e828538e
lqr131115 <1046975211@qq.com>
2024-04-23 10:00:13 +0800

    feat: form

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -1,48 +1,56 @@
 import QNInput from "@/components/QNComponents/QNInput/QNInput";
 import QNRadio from "@/components/QNComponents/QNRadio/QNRadio";
 import QNCheckbox from "@/components/QNComponents/QNCheckbox/QNCheckbox";
+import styles from "@/styles/Question.module.scss";
 
-export default function Question({ post }: any) {
+export default function Question({ id }: any) {
   return (
     <>
-      <QNInput
-        qn_id="qn_input_id"
-        props={{ title: "输入标题", placeholder: "请输入" }}
-      />
-      <QNRadio
-        qn_id="qn_radio_id"
-        props={{
-          title: "单选标题",
-          options: [
-            { value: "A", label: "A" },
-            { value: "B", label: "B" },
-            { value: "C", label: "C" },
-            { value: "D", label: "D" },
-          ],
-          defaultValue: "A",
-          vertical: false,
-        }}
-      />
-      <QNCheckbox
-        qn_id="qn_checkbox_id"
-        props={{
-          title: "多选标题",
-          options: [
-            { value: "A", label: "A" },
-            { value: "B", label: "B" },
-            { value: "C", label: "C" },
-            { value: "D", label: "D" },
-          ],
-          vertical: false,
-        }}
-      />
+      <form>
+        <input type="hidden" name="questionId" value={id} />
+        <QNInput
+          qn_id="qn_input_id"
+          props={{ title: "输入标题", placeholder: "请输入" }}
+        />
+        <QNRadio
+          qn_id="qn_radio_id"
+          props={{
+            title: "单选标题",
+            options: [
+              { value: "A", label: "A" },
+              { value: "B", label: "B" },
+              { value: "C", label: "C" },
+              { value: "D", label: "D" },
+            ],
+            defaultValue: "A",
+            vertical: false,
+          }}
+        />
+        <QNCheckbox
+          qn_id="qn_checkbox_id"
+          props={{
+            title: "多选标题",
+            options: [
+              { value: "A", label: "A" },
+              { value: "B", label: "B" },
+              { value: "C", label: "C" },
+              { value: "D", label: "D" },
+            ],
+            vertical: false,
+          }}
+        />
+        <div className={styles.btnWrapper}>
+          <button className={styles.btn} type="submit">
+            提交
+          </button>
+        </div>
+      </form>
     </>
   );
 }
 
 // 此函数在构建时被调用
 export async function getStaticPaths() {
-  console.log("question/[id].tsx");
   const paths = Array.from({ length: 5 }, (_, i) => ({ id: `id${i}` })).map(
     (post) => ({
       params: { id: post.id },
@@ -52,10 +60,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }: any) {
-  const post = `receive-${params.id}`;
+  const id = `receive-${params.id}`;
   return {
     props: {
-      post,
+      id,
     },
   };
 }
