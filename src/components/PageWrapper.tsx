a26ac5d250c9a58bcc6d68d68b82f4a93b055028
lqr131115 <1046975211@qq.com>
2024-04-24 11:05:44 +0800

    feat: add css and scripts for pageWapper

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
 import Head from "next/head";
+import Script from "next/script";
 import styles from "@/styles/PageWrapper.module.scss";
-import "../app/global.css";
+import "@/styles/global.css";
 
 type PropsType = {
   title: string;
@@ -12,7 +13,7 @@ type PropsType = {
 };
 
 const PageWrapper: FC<PropsType> = (props) => {
-  const { title, description, children } = props;
+  const { title, description, css, scripts, children } = props;
   return (
     <>
       <Head>
@@ -20,8 +21,10 @@ const PageWrapper: FC<PropsType> = (props) => {
         <meta name="description" content={description || title} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
+        <style>{css}</style>
       </Head>
-      <div className={styles.wrapper}>{children}</div>
+      <main className={styles.wrapper}>{children}</main>
+      <Script id="page-js">{scripts}</Script>
     </>
   );
 };
